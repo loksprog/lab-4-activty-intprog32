884ae729d56cfbb37c409b776977f7f0e62158dc
Luke Harvey Umpad <lukeumpad9597@gmail.com>
2026-03-24 09:41:18 +0800

    fix: remove trailing slash from CORS allowed origins

--- role-based-app-backend/server.js
+++ role-based-app-backend/server.js
@@ -9,7 +9,7 @@ const SECRET_KEY = "my-secret-is-my-secret-only";
 
 // Enable cors for frontend
 app.use(cors({
-    origin: ['http://127.0.0.1:5500/', 'http://localhost:5500']
+    origin: ['http://127.0.0.1:5500', 'http://localhost:5500']
 }));
 
 // Middlware to parse JSON
